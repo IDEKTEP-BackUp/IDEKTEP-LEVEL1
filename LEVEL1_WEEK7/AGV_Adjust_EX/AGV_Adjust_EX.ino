da79ca2c63194a7f834caa2d17a0c1507907bdd9
IDEKTEP-backup <idektep.backup.co@gmail.com>
2026-06-28 16:27:54 +0700

    Add files via upload

---
+++ LEVEL1_WEEK7/AGV_Adjust_EX/AGV_Adjust_EX.ino
@@ -0,0 +1,50 @@
+//-------------------------------------------------Declare variable-------------------------------------------------------//
+#define MR_IN1 12 //backright motor
+#define MR_IN2 13 //backright motor
+#define MR_IN3 32 //frontright motor
+#define MR_IN4 33 //frontright motor
+
+#define ML_IN1 27 //frontleft motor
+#define ML_IN2 14 //frontleft motor
+#define ML_IN3 25 //backleft motor
+#define ML_IN4 26 //backleft motor
+
+#define BR_ENA 15  //Pin speed backright motor
+#define FR_ENB 2   //Pin speed frontright motor
+#define BL_ENB 4   //Pin speed backleft motor
+#define FL_ENA 19  //Pin speed frontleft motor
+
+#define L_LED 16  // Left LED
+#define R_LED 17  // Right LED
+
+
+void setup() {
+  pinMode(MR_IN1, OUTPUT);
+  pinMode(MR_IN2, OUTPUT);
+  pinMode(MR_IN3, OUTPUT);
+  pinMode(MR_IN4, OUTPUT);
+  pinMode(ML_IN1, OUTPUT);
+  pinMode(ML_IN2, OUTPUT);
+  pinMode(ML_IN3, OUTPUT);
+  pinMode(ML_IN4, OUTPUT);
+  
+  pinMode(FL_ENA, OUTPUT);
+  pinMode(BL_ENB, OUTPUT);
+  pinMode(BR_ENA, OUTPUT);
+  pinMode(FR_ENB, OUTPUT);
+
+  pinMode(L_LED, OUTPUT);
+  pinMode(R_LED, OUTPUT);
+}
+
+
+void loop() {
+  forward();
+  delay(1000);
+  backward();
+  delay(1000);
+  slide_left();
+  delay(1000);
+  slide_right();
+  delay(1000);
+  }
